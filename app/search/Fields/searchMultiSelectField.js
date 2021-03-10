8742c43b879b32cac9b2a4ae465fb92885ebb8b2
fernando7jr <fernando.settijunior@gmail.com>
2021-03-10 17:18:50 -0300

    fix(fields): override SearchMultiSelectField getNode

--- app/search/Fields/searchMultiSelectField.js
+++ app/search/Fields/searchMultiSelectField.js
@@ -9,6 +9,17 @@ class SearchMultiSelectField extends SearchField {
         this._type = "platformCore";
         this._name = "SearchMultiSelectField";
     }
+
+    getNode() {
+        const node = super.getNode();
+        const type = this._getSoapType();
+        const value = node[type]["platformCore:searchValue"]["$value"];
+        if (typeof value === 'object') {
+            node[type]["platformCore:searchValue"]["$attributes"] = value;
+            node[type]["platformCore:searchValue"]["$value"] = undefined;
+        }
+        return node;
+    }
 }
 
 module.exports = SearchMultiSelectField;
