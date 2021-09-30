b2a2a2e5da9516473fc440422e886656a2a586de
Mauricio Santos <msantos.sw@gmail.com>
2021-09-30 14:47:03 -0300

    adding helper

--- app/record/types/customer.js
+++ app/record/types/customer.js
@@ -1,13 +1,34 @@
-"use strict";
+'use strict';
 
-const Record = require("./common/record");
+const Record = require('./common/record');
+const CustomFieldList = require('../lists/customFieldList');
+const JobAddressbookList = require('../lists/jobAddressbookList');
 
 class Customer extends Record {
-
     constructor() {
         super();
-        this._type = "listRel";
-        this._name = "Customer";
+        this._type = 'listRel';
+        this._name = 'Customer';
+        this._cfList = undefined;
+        this._jobAddressList = undefined;
+    }
+
+    addStringCustomFieldRef(fieldName, value) {
+        if (!this._cfList) {
+            this._cfList = new CustomFieldList('listRel');
+            this.bodyFieldList.push(this._cfList);
+        }
+
+        this._cfList.addStringCustomFieldRef(fieldName, value);
+    }
+
+    addJobAddress(data) {
+        if (!this._jobAddressList) {
+            this._jobAddressList = new JobAddressbookList();
+            this.bodyFieldList.push(this._jobAddressList);
+        }
+
+        this._jobAddressList.addAddressItem(data);
     }
 }
 
